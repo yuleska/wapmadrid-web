e7d57b772f6406b947244b5201e23b700bdd5b1d
yuleska <yuleskaperez@gmail.com>
2015-05-15 12:01:19 +0200

    -

--- js/home.js
+++ js/home.js
@@ -1,18 +1,18 @@
 function getCookie(cname) {
     var name = cname + "=";
     var ca = document.cookie.split(';');
-    for(var i=0; i<ca.length; i++) {
+    for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0)==' ') c = c.substring(1);
+        while (c.charAt(0) == ' ') c = c.substring(1);
         if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
     }
     return "";
 }
 
-function checkCredentials(){
-	var id = getCookie("id");
-	var token = getCookie("token");
-	if (id == "" || token == ""){
-		window.location.href="index.html"
-	}	
+function checkCredentials() {
+    var id = getCookie("id");
+    var token = getCookie("token");
+    if (id == "" || token == "") {
+        window.location.href = "index.html"
+    }
 }
