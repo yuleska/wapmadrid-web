38c9c3178aaa0e8ef68f94a9b4a4ab23b53471ed
Yuleska Perez <yuleskaperez@gmail.com>
2015-05-16 19:09:35 +0200

    funcionando

---
+++ js/walker-list.js
@@ -0,0 +1,45 @@
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
+
+function checkCredentials() {
+    var id = getCookie("id");
+    var token = getCookie("token");
+    if (id == "" || token == "") {
+        window.location.href = "index.html"
+    }
+}
+
+function readCMS_connect(id_in, token_in) {
+    var urlBase = "http://www.proyectowap.tk:3100";
+    var urlRegister = urlBase + "/api/cms/read/" + id_in
+    $.ajax({
+        url: urlRegister,
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        dataType: "json",
+        type: "POST",
+        crossDomain: true,
+        data: {
+            "token": token_in
+        },
+        complete: function(r) {
+            console.log(r);
+            var json = JSON.parse(r.responseText);
+            if (json.error == "0") {
+                $('#CMSname').append(r.responseJSON.name);               
+            } else
+                alert("Error al leer los cambios");
+               
+        },
+        onerror: function(e, val) {
+            alert("No se ha podido realizar la peticion");
+        }
+    });
+}
