bce35ce2d00d96c4f69f8f59050ceed50784aca8
Yuleska Perez <yuleskaperez@gmail.com>
2015-05-16 12:23:00 +0200

    -

--- js/user-profile.js
+++ js/user-profile.js
@@ -16,3 +16,36 @@ function checkCredentials() {
         window.location.href = "index.html"
     }
 }
+
+function readCMS_connect(id_in, token_in) {
+    var urlBase = "http://www.proyectowap.tk:3100";
+    var urlRegister = urlBase + "/api/cms/read/" + id_in
+    $.ajax({
+        url: urlRegister,
+        contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+        dataType: "json",
+        type: "POST",
+        crossDomain: true,
+        data: {
+            "token": token_in
+        },
+        complete: function(r) {
+            console.log(r);
+            var json = JSON.parse(r.responseText);
+            if (json.error == "0") {
+                $('#CMSname1').append(r.responseJSON.name);
+                $('#Username').append(r.responseJSON.username);
+                $('#CMSname2').val(r.responseJSON.name);
+                $('#Telephone').val(r.responseJSON.telephone);
+                $('#OpeningHours').val(r.responseJSON.openingHours);
+                $('#Address').val(r.responseJSON.address);
+                
+            } else
+                alert("Error al leer los cambios");
+                return null;
+        },
+        onerror: function(e, val) {
+            alert("No se ha podido realizar la peticion");
+        }
+    });
+}
