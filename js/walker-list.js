c28f4fd519b70be61071595f68edf6f990a1267d
Yuleska Perez <yuleskaperez@gmail.com>
2015-05-20 18:01:09 +0200

    -

--- js/walker-list.js
+++ js/walker-list.js
@@ -35,7 +35,7 @@ function readCMS_connect(id_in, token_in) {
             if (json.error == "0") {
                 $('#CMSname').append(r.responseJSON.name);               
             } else
-                alert("Error al leer los cambios");
+                alert("Error al leer el centro");
                
         },
         onerror: function(e, val) {
@@ -62,15 +62,14 @@ function readWappies_connect(id_in, token_in) {
             if (json.error == "0") {
                 for (i in r.responseJSON.walkers) {
                     if (i%2 == 0){
-                        $('#listWalkers').append('<tr class="tbl-item" style="background-color:#dbdbdb"><td class=""><div class="row"><div class="col-md-1"><img src="images/avatar/wappy128.png" class="img-responsive" /></div><div class="col-md-9"><p class="title">'+r.responseJSON.walkers[i].walkerID+'</p></div><div class="col-md-2"><a href="walker-edit.html" id="btn_'+r.responseJSON.walkers[i].walkerID+'" class="btn btn-orange">Editar</a></div></div></td></tr>');
+                        $('#listWalkers').append('<tr class="tbl-item" style="background-color:#dbdbdb"><td class=""><div class="row"><div class="col-md-1"><img src="images/avatar/wappy128.png" class="img-responsive" /></div><div class="col-md-9"><p class="title">'+r.responseJSON.walkers[i].displayName+'</p></div><div class="col-md-2"><a href="walker-edit.html" id="btn_'+r.responseJSON.walkers[i].walkerID+'" class="btn btn-orange">Editar</a></div></div></td></tr>');
                     }else{
-                        $('#listWalkers').append('<tr class="tbl-item"><td class=""><div class="row"><div class="col-md-1"><img src="images/avatar/wappy128.png" class="img-responsive" /></div><div class="col-md-9"><p class="title">'+r.responseJSON.walkers[i].walkerID+'</p></div><div class="col-md-2"><a href="walker-edit.html" id="btn_'+r.responseJSON.walkers[i].walkerID+'" class="btn btn-orange">Editar</a></div></div></td></tr>');
+                        $('#listWalkers').append('<tr class="tbl-item"><td class=""><div class="row"><div class="col-md-1"><img src="images/avatar/wappy128.png" class="img-responsive" /></div><div class="col-md-9"><p class="title">'+r.responseJSON.walkers[i].displayName+'</p></div><div class="col-md-2"><a href="walker-edit.html" id="btn_'+r.responseJSON.walkers[i].walkerID+'" class="btn btn-orange">Editar</a></div></div></td></tr>');
 
                     }
-                    console.log(r.responseJSON.walkers[i].walkerID);
                 }              
             } else
-                alert("Error al leer los cambios");
+                alert("Error al leer los wappies");
                
         },
         onerror: function(e, val) {
