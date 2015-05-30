d6ddd766451da562e73ef33f51cd2c8d05b0feb0
yuleska <yuleskaperez@gmail.com>
2015-05-30 15:33:41 +0200

    -

--- js/cms-edit.js
+++ js/cms-edit.js
@@ -47,11 +47,11 @@ function readallRoutes_connect() {
     });
 }
 
-function readallCMS_connect(id_in, token_in) {
+function readCMS_connect(id_in, token_in) {
     var urlBase = "http://www.proyectowap.tk:3100";
-    var urlRegister = urlBase + "/api/admin/cms/read/all/" + id_in
+    var urlCMS = urlBase + "/api/admin/cms/read/" + id_in
     $.ajax({
-        url: urlRegister,
+        url: urlCMS,
         contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
         dataType: "json",
         type: "POST",
@@ -63,31 +63,14 @@ function readallCMS_connect(id_in, token_in) {
             console.log(r);
             var json = JSON.parse(r.responseText);
             if (json.error == "0") {
-                for (i in r.responseJSON.user) {
-                    console.log(r.responseJSON.user[i].name);
-                    if (i % 2 == 0) {
-                        $('#listallCMS').append('<tr class="tbl-item" style="background-color:#dbdbdb"><td class=""><div class="row"><div class="col-md-1"><img src="images/avatar/cms.jpg" class="img-responsive" /></div><div class="col-md-9"><p class="title">' + r.responseJSON.user[i].name + '</p></div><div class="col-md-2"><a onclick="readCMS_connect(this.id)" href="#" id="btn_' + r.responseJSON.user[i]._id + '" class="btn btn-orange">Editar</a></div></div></td></tr>');
-                    } else {
-                        $('#listallCMS').append('<tr class="tbl-item"><td class=""><div class="row"><div class="col-md-1"><img src="images/avatar/cms.jpg" class="img-responsive" /></div><div class="col-md-9"><p class="title">' + r.responseJSON.user[i].name + '</p></div><div class="col-md-2"><a href="cms-edit.html"  onclick="readCMS_connect(this.id)" id="btn_' + r.responseJSON.user[i]._id + '" class="btn btn-orange">Editar</a></div></div></td></tr>');
-
-                    }
-                }
+                alert("here yuli");
             } else
-                alert("Error al leer los wappies");
-
+                alert("Error al leer el CMS");
         },
         onerror: function(e, val) {
             alert("No se ha podido realizar la peticion");
         }
     });
-}
-
-function readCMS_connect(id_in) {
-    var long = id_in.length;
-    var id = id_in.substring(4,long);
-    alert(id);
-    document.cookie="userId="+id;
-    window.location.href = "cms-edit.html";
 }
 
 function logout_connect(id_in, token_in) {
